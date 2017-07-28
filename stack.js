0c5b98b84ce0ae7467974d4da2c5718e5759fd4d
Jamie Wang <sephona@gmail.com>
2017-07-28 13:03:00 -0400

    basic parenthesis exercise finished

--- stack.js
+++ stack.js
@@ -43,33 +43,33 @@ const peek = () => {
     //if the top of the stack does not have anything 
     //then the stack is empty
     //otherwise return what's on the top
-  if (s.top === null) {
+  if (stack.top === null) {
     return null;
   }
-  return s.top.data;
+  return stack.top.data;
 };
 
 const display = () => {
     // displays the entire contents of the stack
-  let node = s.top;
+  let node = stack.top;
   while (node !== null) {
     console.log(node.data);
     node = node.next;
   }
 };
     
-let s = new Stack();
+let stack = new Stack();
  
-s.push(1);
-s.push(2);
-s.push('Jamie');
-console.log('Top of stack:', peek());//Jamie
+// s.push(1);
+// s.push(2);
+// s.push('Jamie');
+// console.log('Top of stack:', peek());//Jamie
 
-s.pop();//Jamie was popped out....orphan...sad
-s.push('Saule');
-console.log('Top of stack:', peek());//Saule
+// s.pop();//Jamie was popped out....orphan...sad
+// s.push('Saule');
+// console.log('Top of stack:', peek());//Saule
 
-display();//Saule, 2, 1
+// display();//Saule, 2, 1
 
 // Palindromes
 
@@ -78,25 +78,84 @@ display();//Saule, 2, 1
 // Write a function that takes a string of letters and returns true or false to determine whether it is palindromic. For example:
 
 function is_palindrome(s) {
-    s = s.toLowerCase().replace(/[^a-z]/g, "");
+  s = s.toLowerCase().replace(/[^a-z]/g, '');
     //s="dad";
-    let stack = new Stack();
-    for (let i=0; i < s.length; i++){
-        stack.push(s.charAt(i));
-    }
-    let reverse = '';
-    while (stack.top !== null){
-        reverse += stack.pop();
-    }
-    if (reverse === s) {
-        return true;
-    }
-    return false;
+  for (let i=0; i < s.length; i++){
+    stack.push(s.charAt(i));
+  }
+  let reverse = '';
+  while (stack.top !== null){
+    reverse += stack.pop();
+  }
+  if (reverse === s) {
+    return true;
+  }
+  return false;
   //
 }
 
 // true, true, true
-//console.log(is_palindrome("dad"));
+// console.log(is_palindrome("dad"));
 //console.log(is_palindrome("A man, a plan, a canal: Panama"));
-console.log(is_palindrome("cat"));
-console.log(is_palindrome("1001"));
+// console.log(is_palindrome('cat'));
+// console.log(is_palindrome('1001'));
+
+
+// Matching parentheses in an expression
+
+// A stack can be used to ensure that an arithmetic expression has balanced parentheses. Write a function that takes an arithmetic expression as an argument and returns the position in the expression where a parenthesis is missing or incorrect.
+
+// For version 1, the parentheses you need to consider are ( and ). Finding a close parenthesis without an open parenthesis is an error (report the location of the close); reaching the end of the string while still "holding" an open parenthesis is also an error (report the location of the open).
+
+//if we have (1 + 2 * 3 ....then we want to know that there is a missing ')', AND, if there is 1 + 2 *3)...that there is no '('.
+
+const matchingParenthesis = (str) => {
+  //str = '(1 + 2 * 3';
+  let open = 0;
+  let close = 0;
+  let openLocation = '';
+  let closeLocation = '';
+  
+  // for(let i = 0; i < str.length; i++) {
+  //   if(str.charAt(i) === '(') {
+  //     open += 1;
+  //     openLocation = i;
+  //     stack.push(str.charAt(i));
+  //   } else if (str.charAt(i) === ')') {
+  //     close += 1;
+  //     closeLocation = i;
+  //     stack.push(str.charAt(i));
+  //   }
+  // }
+
+  for(let i = 0; i < str.length; i++) {
+    stack.push(str.charAt(i));
+    if(peek() === '(') {
+      open += 1;
+      openLocation = i;
+    } else if (peek() === ')') {
+      close += 1;
+      closeLocation = i;
+    }
+  }
+
+  if(open < close) {
+    return ('Missing open parenthesis, close parenthesis at index ' + closeLocation);
+  } else if (close < open) {
+    return ('Missing close parenthesis, open parenthesis at index ' + openLocation);
+  } else {
+    return ('All is well!');
+  }
+
+};
+
+console.log(matchingParenthesis('(1 + 2 * 3'));
+console.log(matchingParenthesis('1 + 2 * 3)'));
+console.log(matchingParenthesis('(1 + 2 * 3)'));
+
+// Extension exercise: Recognize three pairs of brackets: (), [], and {}. These must be correctly nested; "([)]" is incorrect, and should report an error at the ), stating that you were expecting a ] but found a ). If this is starting to look and sound very familiar, congratulations - you're beginning to write a simple language parser!
+
+
+
+// Extension extension exercise: Also recognize two types of quote character: "" and ''. Inside quotes, brackets aren't counted at all - in fact, nothing is counted until you reach the corresponding close quote.
+
